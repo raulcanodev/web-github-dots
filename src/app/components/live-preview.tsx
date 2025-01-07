5ac06d250fef31b05d63af330eaf188c56a15273
Raul Marco Cano <rawraul@outlook.com>
2025-01-07 17:15:23 +0000

    styles

--- src/app/components/live-preview.tsx
+++ src/app/components/live-preview.tsx
@@ -5,13 +5,13 @@ export default function LivePreview() {
   const token = process.env.NEXT_PUBLIC_GITHUB_PERSONAL_ACCESS_TOKEN || '';
 
   return (
-    <section className="container px-4 py-16 m-auto flex justify-center">
+    <section className="container px-8 py-16 m-auto flex justify-center">
       <div className="space-y-6">
-          <ContributionGraph
-            username="raulcanodev"
-            theme="dark"
-            token={token}
-          />
+        <ContributionGraph
+          username="raulcanodev"
+          theme="dark"
+          token={token}
+        />
         <p className="text-sm text-neutral-400">
           Live preview of the contribution graph component using real GitHub data.
         </p>
